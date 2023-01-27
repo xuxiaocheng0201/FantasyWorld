099a5eb67e3d002afd13dbdade576cb2e9e1620d
xuxiaocheng0201 <2762267080@qq.com>
2023-01-28 03:06:35 +0800

    feat(FantasyWorld): 处理参数
    处理传入参数，允许runServet，runClient，hideWindow，showWindow。除此之外将输出WARN

--- src/main/java/com/xuxiaocheng/FantasyWorld/Platform/FantasyWorldPlatform.java
+++ src/main/java/com/xuxiaocheng/FantasyWorld/Platform/FantasyWorldPlatform.java
@@ -41,33 +41,54 @@ public final class FantasyWorldPlatform {
 
     private static @NotNull HLog logger = HLog.DefaultLogger;
     static {
-        final File dir = new File("logs");
-        final File path = new File(dir, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss")) + ".log");
-        try {
-            if (!dir.exists()) {
-                if (!dir.mkdirs())
-                    throw new IOException("Failed to create log directory.");
-            } else if (!dir.isDirectory())
-                throw new IOException("Not a log directory.");
-            if (path.exists() && !path.isFile())
-                throw new IOException("Not a log file.");
-            if (!path.createNewFile())
-                     throw new IOException("Failed to create log file.");
+        if (!HLog.isDebugMode()) {
+            final File dir = new File("logs");
+            final File path = new File(dir, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss")) + ".log");
             try {
-                FantasyWorldPlatform.logger = HLog.createInstance("DefaultFileLogger", false,
-                        new BufferedOutputStream(new FileOutputStream(path, true)));
-            } catch (final FileNotFoundException exception) {
-                throw new IOException("Failed to get log file.", exception);
+                if (!dir.exists()) {
+                    if (!dir.mkdirs())
+                        throw new IOException("Failed to create log directory.");
+                } else if (!dir.isDirectory())
+                    throw new IOException("Not a log directory.");
+                if (path.exists() && !path.isFile())
+                    throw new IOException("Not a log file.");
+                if (!path.createNewFile())
+                    throw new IOException("Failed to create log file.");
+                try {
+                    FantasyWorldPlatform.logger = HLog.createInstance("DefaultFileLogger", false,
+                            new BufferedOutputStream(new FileOutputStream(path, true)));
+                } catch (final FileNotFoundException exception) {
+                    throw new IOException("Failed to get log file.", exception);
+                }
+            } catch (final IOException exception) {
+                HLog.DefaultLogger.log(HLogLevel.FAULT, "Fail to create log file.");
+                HLog.DefaultLogger.log(HLogLevel.FAULT, exception);
             }
-        } catch (final IOException exception) {
-            HLog.DefaultLogger.log(HLogLevel.FAULT, "Fail to create log file.");
-            HLog.DefaultLogger.log(HLogLevel.FAULT, exception);
         }
     }
 
-    public static void main(final String[] args) {
+    private static boolean runOnClient = false;
+    private static boolean showJWindow = false;
+    public static boolean isRunOnClient() {
+        return FantasyWorldPlatform.runOnClient;
+    }
+    public static boolean isShowJWindow() {
+        return FantasyWorldPlatform.showJWindow;
+    }
+
+    public static void main(@NotNull final String @NotNull [] args) {
         Thread.currentThread().setName("FantasyWorldPlatform/main");
+        Thread.setDefaultUncaughtExceptionHandler((thread, error) -> FantasyWorldPlatform.logger.log(HLogLevel.FAULT, "An uncaught exception has been thrown in thread '" + thread + "'.", error));
         Runtime.getRuntime().addShutdownHook(new ShutdownHookThread(Thread.currentThread().getName()));
         FantasyWorldPlatform.logger.log(HLogLevel.FINE, "Hello FantasyWorld platform! ", FantasyWorldPlatform.CurrentVersion);
+        for (final String arg: args) {
+            switch (arg.hashCode()) {
+                case -826834514 -> FantasyWorldPlatform.runOnClient = false; //"runServer"
+                case -1278720458 -> FantasyWorldPlatform.runOnClient = true; //"runClient"
+                case 455289810 -> FantasyWorldPlatform.showJWindow = false; //"hideWindow"
+                case 887337293  -> FantasyWorldPlatform.showJWindow = true; //"showWindow"
+                default -> FantasyWorldPlatform.logger.log(HLogLevel.WARN, "Unrecognized arg: ", arg);
+            }
+        }
     }
 }
